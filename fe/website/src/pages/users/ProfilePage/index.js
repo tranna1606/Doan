37e15bd73676d075b44e08f185d8a2f8dcdb8925
2151053040na@ou.edu.vn <2151053040na@ou.edu.vn>
2024-09-11 14:02:27 +0700

    b3

---
+++ fe/website/src/pages/users/ProfilePage/index.js
@@ -0,0 +1,10 @@
+import { memo } from 'react';
+
+const ProfilePage = () => {
+    return (
+        <>
+            <h1>Content ProfilePage</h1>
+        </>
+    );
+};
+export default memo(ProfilePage);
