a76edbb524c52c0d2083a033bb1b2a417fc1f422
2151053040na@ou.edu.vn <2151053040na@ou.edu.vn>
2024-09-12 17:27:25 +0700

    slider

--- fe/website/src/pages/users/homePage/index.js
+++ fe/website/src/pages/users/homePage/index.js
@@ -1,9 +1,72 @@
 import { memo } from 'react';
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import item1 from 'assets/users/images/slider/item1.jpg';
+import item2 from 'assets/users/images/slider/item2.jpg';
+import item3 from 'assets/users/images/slider/item3.jpg';
+import item4 from 'assets/users/images/slider/item4.jpg';
+import item5 from 'assets/users/images/slider/item5.jpg';
+import './style.scss';
 const HomePage = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
+
+    const sliderItems = [
+        {
+            bgImg: item1,
+            name: 'Đầm',
+        },
+        {
+            bgImg: item2,
+            name: 'Chân váy',
+        },
+        {
+            bgImg: item3,
+            name: 'Áo thun',
+        },
+        {
+            bgImg: item4,
+            name: 'Áo sơ mi',
+        },
+        {
+            bgImg: item5,
+            name: 'Set áo váy',
+        },
+    ];
     return (
         <>
-            <h1>Content HomePage</h1>
+            {/* Categories Begin */}
+            <div className="container container__categories_slider">
+                <Carousel responsive={responsive} className="categories_slider">
+                    {sliderItems.map((item, key) => (
+                        <div
+                            className="categories_slider_item"
+                            style={{ backgroundImage: `url(${item.bgImg})` }}
+                            key={key}
+                        >
+                            <p>{item.name}</p>
+                        </div>
+                    ))}
+                </Carousel>
+            </div>
+            {/* Categories End */}
         </>
     );
 };
